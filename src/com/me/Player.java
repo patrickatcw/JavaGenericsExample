a0dc227d34e49f86d50fd0d101793d2989d79b27
patrickatcw <pj8392@yahoo.com>
2018-03-29 11:31:03 -0400

    player class

---
+++ src/com/me/Player.java
@@ -0,0 +1,23 @@
+package com.me;
+
+//step 1 create class
+//step 2 make it into an abstract class
+public abstract class Player {
+
+    //step 3 variables / field
+    private String name;
+
+    //step 4 constructor
+    public Player(String name) {
+        this.name = name;
+    }
+
+    //step 5 getter
+    public String getName() {
+        return name;
+
+        //step 6 create baseballplayer class
+
+    }
+}
+
