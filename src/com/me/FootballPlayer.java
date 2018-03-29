09486dbf896cf15417784e3ed884758f5ed4efad
patrickatcw <pj8392@yahoo.com>
2018-03-29 11:34:21 -0400

    football player class

---
+++ src/com/me/FootballPlayer.java
@@ -0,0 +1,15 @@
+package com.me;
+
+//step 9 create this class
+//step 10 extends player
+public class FootballPlayer extends Player{
+
+    //step 11 constructor
+    public FootballPlayer(String name) {
+        super(name);
+
+        //step 12 create soccerplayer class
+
+
+    }
+}
