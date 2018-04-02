a9d512fd69084fa3edbd46cbe20e73fc80880ae8
patrickatcw <pj8392@yahoo.com>
2018-04-02 10:28:06 -0400

    finished project, ran, results commented at bottom of main

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -2,8 +2,155 @@ package com.me;
 
 public class Main {
 
+    /*public static void main(String[] args) {
+
+        //arraylist example shown before generics example
+        //step 1 variables, note Integer, diamonds
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(1);
+        items.add(2);
+        //can't put this in, error occurs
+        //b/c trying to add a string to an arraylist object
+        //items.add("Patrick");
+        items.add(3);
+        items.add(4);
+        items.add(5);
+
+        //sttep 2 calling the method
+        printDoubled(items);
+
+    }
+
+        //step 3 method
+    private static void printDoubled(ArrayList<Integer> n){
+
+        for(int i : n){
+
+            System.out.println(i * 2);
+
+            //step 4 run this
+            *//*
+            results;
+            2
+            4
+            6
+            8
+            10
+             *//*
+
+        }
+
+    }*/
+
     public static void main(String[] args) {
+
+        //step 24 instances for players
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer ("Beckham");
+
+        //step 25 instances for teams
+        //Team adelaideCrows = new Team("Adelaide Crows");
+
+        //step 33 change the above
+        Team <FootballPlayer>adelaideCrows = new Team<>("Adelaide Crows");
+
+        //step 26 calling method to add player to team
+        adelaideCrows.addPlayer(joe);
+        //commented out for step 34
+        /*adelaideCrows.addPlayer(pat);
+        adelaideCrows.addPlayer(beckham);*/
+
+        //step 27 print
+        System.out.println(adelaideCrows.numPlayers());
+        /*
+        results;
+            Joe picked for team Adelaide Crows
+            Pat picked for team Adelaide Crows
+            Beckham picked for team Adelaide Crows
+            3*/
+        //problem is we need a check for type of player goes on
+        // right kind of team
+        //solution - generics, modify team to be a generic class
+
+        //step 28 go to team class
+
+        //step 34 add a baseball team class
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+
+        //step 35 validation
+        //Team<String> brokenTeam = new Team<>("this won't work");
+        //brokenTeam.addPlayer("no-one");
+        //step 37 change above to....
+        Team<SoccerPlayer> brokenTeam = new Team<>("this won't work");
+        brokenTeam.addPlayer(beckham);
+
+        //step 38 calls for adding and results
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        FootballPlayer banks = new FootballPlayer("Gordan");
+        melbourne.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorne = new Team<>("Hawthorne");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        hawthorne.matchResult(fremantle, 1, 0);
+        hawthorne.matchResult(adelaideCrows, 3, 8);
+
+        adelaideCrows.matchResult(fremantle, 2, 1);
+        //adelaideCrows.matchResult(baseballTeam, 2, 1);
+        //step 44 comment above out because we get error on baseballTeam
+
+        //step 45 print rankings
+        System.out.println("Rankings");
+        System.out.println(adelaideCrows.getName() + ": " + adelaideCrows.ranking());
+        System.out.println(melbourne.getName() + ": " + melbourne.ranking());
+        System.out.println(fremantle.getName() + ": " + fremantle.ranking());
+        System.out.println(hawthorne.getName() + ": " + hawthorne.ranking());
+        //step 46 run
+
+        //step 47 back to team for comparing
+
+        //step 50 back from team
+        System.out.println(adelaideCrows.compareTo(melbourne));
+        System.out.println(adelaideCrows.compareTo(hawthorne));
+        System.out.println(hawthorne.compareTo(adelaideCrows));
+        System.out.println(melbourne.compareTo(fremantle));
+        //step 51 run and done...finally!!!
+
+        //step 39 run
+
+        //step 40 put message in team class in matchResult method
+
+        //step 42 run, back from team step 41
+
+        //step 43 back to team to code for matching teams for playing each other
+
+        //step 36 got to team class
 
     }
 
 }
+
+
+/*
+final results;
+Joe picked for team Adelaide Crows
+1
+Pat picked for team Chicago Cubs
+Beckham picked for team this won't work
+Gordan picked for team Melbourne
+Hawthorne beat Fremantle
+Hawthorne lost to Adelaide Crows
+Adelaide Crows beat Fremantle
+Rankings
+Adelaide Crows: 4
+Melbourne: 0
+Fremantle: 0
+Hawthorne: 2
+-1
+-1
+1
+0
+ */
+
--- src/com/me/Team.java
+++ src/com/me/Team.java
@@ -1,5 +1,134 @@
 package com.me;
 
-//step 15 create this team
-public class Team {
+
+//T is type
+
+import java.util.ArrayList;
+
+//step 15 create this class
+//public class Team {
+
+//step 28 modify team class
+//public class Team<T>{
+
+//step 36 extends Player, then change back steps 31 and 32 below, then back to main step 37
+//public class Team<T extends Player>{
+
+//step 47 change for comparing teams, then step 48 comparable method below
+public class Team<T extends Player> implements Comparable<Team<T>> {
+
+    //variables
+    int played = 0;
+    int won = 0;
+    int lost = 0;
+    int tied = 0;
+
+    //step 16 field
+    private String name;
+
+    //step 17 create an arraylist using generics
+    //for our player object
+    //private ArrayList<Player> members = new ArrayList<>();
+
+    //step 29 modify above
+    private ArrayList<T> members = new ArrayList<>();
+
+    //step 18 constructor
+    public Team(String name) {
+        this.name = name;
+    }
+
+    //step 19 getter
+    public String getName() {
+        return name;
+    }
+
+    //step 20 method to add player
+    //public boolean addPlayer(Player player) {
+
+    //step 30 Player to T
+    public boolean addPlayer(T player) {
+        if (members.contains(player)) {
+            System.out.println(player.getName() + " is already on this team");
+            //step 31 change getName because we get an error
+            //System.out.println(((Player)(player)).getName() + " is already on this team");
+            return false;
+        } else {
+            members.add(player);
+            System.out.println(player.getName() + " picked for team " + this.name);
+            //step 32 change getName because we get an error
+            //System.out.println(((Player)(player)).getName() + " picked for team " + this.name);
+            //step 33 go back to main method
+
+            return true;
+        }
+
+    }
+
+    //step 21 method to return size of arraylist
+    public int numPlayers() {
+        return this.members.size();
+
+    }
+
+    //step 22 method to get result of games
+    //public void matchResult(Team opponent, int ourScore, int theirScore) {
+
+    //step 43 change the above for generic type
+    public void matchResult(Team<T> opponent, int ourScore, int theirScore) {
+        //step 44 back to main
+
+        //step 40 message
+        String message;
+
+        if (ourScore > theirScore) {
+            won++;
+            //step 40 message
+            message = " beat ";
+        } else if (ourScore==theirScore) {
+            tied++;
+            //step 40 message
+            message = " drew with ";
+        } else {
+            lost++;
+            //step 40 message
+            message = " lost to ";
+        }
+        played++;
+        if (opponent!=null) {
+            //step 41 add the sout below
+            System.out.println(this.getName() + message + opponent.getName());
+            //step 42 back to main to run
+            opponent.matchResult(null, theirScore, ourScore);
+        }
+
+    }
+
+    //step 23 method for ranking
+    public int ranking() {
+        return (won * 2) + tied;
+
+
+        //step 24 to main method
+
+    }
+
+    //step 48 compare method, implement method
+    @Override
+    public int compareTo(Team<T> team) {
+        //step 49
+        if(this.ranking() > team.ranking()){
+            return -1;
+        }else if(this.ranking() < team.ranking()){
+            return 1;
+        }else {
+            return 0;
+
+            //step 50 back to main
+
+        }
+    }
 }
+
+
+
